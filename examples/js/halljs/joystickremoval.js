97ae790e91fec64d7b7ab14dd1d5d1a2da9cb37c
VENKATA SAI <venkatasaib@smbxl.com>
2024-04-20 22:54:41 +0530

    joystick-changes-desktop

--- examples/js/halljs/joystickremoval.js
+++ examples/js/halljs/joystickremoval.js
@@ -1,10 +1,13 @@
+// var parser = new UAParser();
+// var result = parser.getResult();
+// var useragent =result.device.type
+// var os =result.os.name
+// if(useragent=="mobile"){
 
-var parser = new UAParser();
-var result = parser.getResult();
-var useragent =result.device.type
-var os =result.os.name
-if(useragent=="mobile"){
-
-}else{
-  document.querySelector(".scenea").removeAttribute('joystick');
-}
+// }else{
+//  // alert("e")
+//   // document.querySelector(".scenea").removeAttribute('joystick');
+//   document.querySelector('#np').style.right = '165px';
+//   document.querySelector('#np').style.bottom = '20px';
+  
+// }
